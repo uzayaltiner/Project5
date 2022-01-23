09b61af78dfd667c18cd2b294a5a9ddbffc5c838
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-01-23 17:32:00 +0300

    Challenges done

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -27,7 +27,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -51,41 +51,30 @@ class ViewController: UITableViewController {
             [weak self, weak ac] _ in
             guard let answer = ac?.textFields?[0].text else { return}
             self?.submit(answer)
-       }
+        }
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer) {
-           if isOriginal(word: lowerAnswer) {
-             if isReal(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
                     usedWords.insert(answer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
-                 
-                 return
-             } else {
-                 errorTitle = "Word not recognized"
-                 errorMessage = "You can't just make them up, and you know it!"
-             }
-           } else {
-               errorTitle = "Word already used"
-               errorMessage = "Be an original"
-           }
+                    
+                    return
+                } else {
+                    showErrorMessage("Word not recognized", "You can't just make them up, and you know it!")
+                }
+            } else {
+                showErrorMessage("Word already used", "Be an original")
+            }
         } else {
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title!.lowercased())"
+            showErrorMessage("Word not possible", "You can't spell that word from \(title!.lowercased())")
         }
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-        
-        
     }
     
     func isPossible(word: String) -> Bool {
@@ -102,14 +91,23 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
+        guard word != title else { return false }
+        
         return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
+        guard word.count > 3 else {return false}
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
+    }
+    
+    func showErrorMessage(_ errorTitle: String, _ errorMessage: String){
+        let ac = UIAlertController(title: errorTitle , message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
     }
 }
 
